51623708ca44b09742cf628f7bbd08544f92ae28
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-02-08 01:00:34 +0530

    test update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <section>
-        <h1 className="text-4xl font-bold mt-12">NEXT.js Image Gallery</h1>
+        <h1 className="text-4xl font-bold mt-12">NEXT.js Image Gallery V2</h1>
         <p className="text-lg mt-6 bg-sky-600 p-6 rounded-lg">
           This page <strong>fetches and caches data at build time</strong>. Even
           though the Unsplash API always returns a new image, we see the same
