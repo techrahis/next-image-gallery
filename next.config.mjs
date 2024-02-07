15e4ea75028f75ddce73179b92044638b8eb329f
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-02-08 00:46:28 +0530

    project init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "plus.unsplash.com",
+      "source.unsplash.com",
+    ],
+  },
+};
 
 export default nextConfig;
